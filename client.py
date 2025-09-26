04a2c29d5f7ea6595a99f2536b195c96efe7bd0b
Devon O'Quinn <doquinn3@gatech.edu>
2025-09-25 20:35:16 -0400

    implemented argparse

--- client.py
+++ client.py
@@ -3,5 +3,23 @@ import threading
 import sys
 import argparse
 
+def main():
+    parser = argparse.ArgumentParser(description="Chat Client")
+    parser.add_argument('-join', action='store_true', help='Join the chat server')
+    parser.add_argument('-host', type=str, required=True, help='Server IPv4 address')
+    parser.add_argument('-port', type=int, required=True, help='Server port')
+    parser.add_argument('-username', type=str, required=True, help='Display name, max 8 chars')
+    parser.add_argument('-passcode', type=str, required=True, help='Passcode, max 5 chars')
+    args = parser.parse_args()
+    if not args.join:
+        sys.exit(1)
+    # end if
+    if len(args.username) > 8:
+        sys.exit(1)
+    # end if
 
-# TODO: Implement a client that connects to your server to chat with other clients here
+# end main
+
+if __name__ == "__main__":
+    main()
+# end if
--- server.py
+++ server.py
@@ -4,4 +4,34 @@ import sys
 import argparse
 from datetime import datetime, timedelta
 
-#TODO: Implement all code for your server here
+def main():
+    parser = argparse.ArgumentParser(description="CS3251 Chat Server")
+    parser.add_argument('-start', action='store_true', help='Start the server')
+    parser.add_argument('-port', type=int, required=True, help='Listening port')
+    parser.add_argument('-passcode', type=str, required=True, help='Shared passcode')
+    args = parser.parse_args()
+    if not args.start:
+        sys.exit(1)
+    # end if
+    if len(args.passcode) > 5:
+        sys.exit(1)
+    # end if
+    if 1 <= args.port <= 65535:
+        sys.exit(1)
+    # end if
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('127.0.0.1', args.port))
+    server.listen()
+    print(f"Server started on port {args.port}. Accepting connections")
+    sys.stdout.flush()
+    while True:
+        connection, address = server.accept()
+
+        connection.close()
+    # end while
+# end main
+
+if __name__ == "__main__":
+    main()
+# end if
